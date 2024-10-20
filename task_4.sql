55dd44ffec9768642a5898f63825fef8f6b4cd79
utibe solomon <utibesolomon6@gmail.com>
2024-10-20 15:06:59 +0100

    Correction

--- task_4.sql
+++ task_4.sql
@@ -2,12 +2,13 @@ USE alx_book_store;
 SELECT 
     COLUMN_NAME AS 'Column Name',
     DATA_TYPE AS 'Data Type',
+    COLUMN_TYPE AS 'Column Type',
     CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
     IS_NULLABLE AS 'Nullable',
     COLUMN_DEFAULT AS 'Default Value'
 FROM 
-    information_schema.COLUMNS
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_NAME = 'Books' 
-    AND TABLE_SCHEMA = DATABASE();
+    AND TABLE_SCHEMA = 'alx_book_store';
     
