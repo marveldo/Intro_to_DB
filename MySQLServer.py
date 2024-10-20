61f0b7c45f94178cda1336fbd9bc9e5fa13f8a28
utibe solomon <utibesolomon6@gmail.com>
2024-10-20 14:59:12 +0100

    Correction

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from mysql.connector import Error
+
 
 def create_database():
     try:
@@ -20,7 +20,7 @@ def create_database():
             # Print success message
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         # Print error message if there's a failure
         print(f"Error while connecting to MySQL: {e}")
 
